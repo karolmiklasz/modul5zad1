1797c11c58c9096bc3873817ac08d35c86977782
Karol Miklasz <karol3wpl@gmail.com>
2023-10-30 22:56:35 +0100

    Define BaseContact class

---
+++ main.py
@@ -0,0 +1,15 @@
+from faker import Faker
+
+fake = Faker()
+
+class BaseContact:
+    def __init__(self, first_name, last_name, phone, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone = phone
+        self.email = email
+    
+    def contact(self):
+        print(f"Wybieram numer {self.phone} i dzwonię do {self.first_name} {self.last_name}")
+    
+    
